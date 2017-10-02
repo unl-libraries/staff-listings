c396daa7aeafad59a5fbc4651432402256407080
srickel1 <srickel1@LIB17003.unl.edu>
2017-10-02 15:47:12 -0500

    Removing unintended directory commit

--- Directory/subject_librarians_view.sql
+++
@@ -1,6 +0,0 @@
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `subject_librarians` AS 
-select `subjects`.`subject` AS `subject`,
-`library_people`.`userid` AS `userid`,
-`library_people`.`first_name` AS `first_name`,
-`library_people`.`last_name` AS `last_name` from (((`library_people` join `subjects`) join `subject_assignments`) join `library_data`)
-where ((`library_data`.`id` = `subject_assignments`.`person_id`) and (`subject_assignments`.`subject_id` = `subjects`.`id`) and (`library_data`.`nuid` = `library_people`.`nuid`)) order by `subjects`.`subject`
